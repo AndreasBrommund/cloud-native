1b1471f0b01a1de0a65e72de6e8e0c58c1d8df69
Wilhelm Hedman <wilhelm.hedman@omegapoint.se>
2019-06-17 17:43:26 +0200

    Added new account script

---
+++ accounts/organization-new-acc.sh
@@ -0,0 +1,116 @@
+Copyright 2008-2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+    http://aws.amazon.com/apache2.0/
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+Adaption of https://aws.amazon.com/blogs/security/how-to-use-aws-organizations-to-automate-end-to-end-account-creation/
+
+#!/bin/bash
+function usage
+{
+    echo "usage: organization_new_acc.sh [-h] --account_name ACCOUNT_NAME
+                                      --account_email ACCOUNT_EMAIL
+                                      --cl_profile_name CLI_PROFILE_NAME
+                                      [--user_first_name USER_FIRST_NAME]
+                                      [--region AWS_REGION]"
+}
+
+userFirstName=""
+password=$(date +%s | sha256sum | base64 | head -c 10)
+newAccName=""
+newAccEmail=""
+newProfile=""
+roleName="OrganizationAccountAccessRole"
+destinationOUId="ou-bdes-ffer7sda"
+region="eu-west-1"
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -n | --account_name )   shift
+                                newAccName=$1
+                                ;;
+        -e | --account_email )  shift
+                                newAccEmail=$1
+                                ;;
+        -p | --cl_profile_name ) shift
+                                newProfile=$1
+                                ;;
+        -u | --user_first_name ) shift
+                                userFirstName=$1
+                                ;;
+        -r | --region )        shift
+                                region=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+    esac
+    shift
+done
+
+if [ "$newAccName" = "" ] || [ "$newAccEmail" = "" ] || [ "$newProfile" = "" ]
+then
+  usage
+  exit
+fi
+
+printf "Create New Account\n"
+ReqID=$(aws organizations create-account --email $newAccEmail --account-name "$newAccName" --role-name $roleName \
+--query 'CreateAccountStatus.[Id]' \
+--output text)
+
+printf "Waiting for New Account ..."
+orgStat=$(aws organizations describe-create-account-status --create-account-request-id $ReqID \
+--query 'CreateAccountStatus.[State]' \
+--output text)
+
+while [ $orgStat != "SUCCEEDED" ]
+do
+  if [ $orgStat = "FAILED" ]
+  then
+    printf "\nAccount Failed to Create\n"
+    exit 1
+  fi
+  printf "."
+  sleep 10
+  orgStat=$(aws organizations describe-create-account-status --create-account-request-id $ReqID \
+  --query 'CreateAccountStatus.[State]' \
+  --output text)
+done
+
+accID=$(aws organizations describe-create-account-status --create-account-request-id $ReqID \
+--query 'CreateAccountStatus.[AccountId]' \
+--output text)
+
+printf "Account id: $accID\n"
+
+accARN="arn:aws:iam::$accID:role/$roleName"
+
+printf "\nCreate New CLI Profile\n"
+aws configure set region $region --profile $newProfile
+aws configure set role_arn $accARN --profile $newProfile
+aws configure set source_profile default --profile $newProfile
+
+
+printf "Create IAM account\n"
+aws iam create-user --user-name $userFirstName --profile $newProfile > /dev/null 2>&1
+aws iam create-login-profile --user-name $userFirstName --password $password --password-reset-required --profile $newProfile > /dev/null 2>&1
+aws iam attach-user-policy --user-name $userFirstName --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --profile $newProfile > /dev/null 2>&1
+
+
+if [ "$destinationOUId" != "" ]
+then
+  printf "Moving New Account to OU\n"
+  rootOU=$(aws organizations list-roots --query 'Roots[0].[Id]' --output text)
+
+  aws organizations move-account --account-id $accID --source-parent-id $rootOU --destination-parent-id $destinationOUId > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+    printf "Moving Account Failed\n"
+  fi
+fi
+
+printf "$newAccEmail\t$accID\t$userFirstName\t$password\n"
